e33addeb8d4910fb1ac83c0e4524826b5762784f
vntu98 <vntu98@gmail.com>
2021-10-02 21:36:21 +0700

    Design Idea

---
+++ resources/views/index.blade.php
@@ -0,0 +1,75 @@
+<x-app-layout>
+    <div class="filters flex space-x-6">
+        <div class="w-1/3">
+            <select name="category" id="category" class="w-full rounded-xl border-none px-4 py-2">
+                <option value="one">Category One</option>
+                <option value="two">Category Two</option>
+            </select>
+        </div>
+        <div class="w-1/3">
+            <select name="other_filters" id="other_filters" class="w-full rounded-xl border-none px-4 py-2">
+                <option value="one">Category One</option>
+                <option value="two">Category Two</option>
+            </select>
+        </div>
+        <div class="w-2/3 relative">
+            <input type="search" placeholder="Find an ideas" class="w-full border-none placeholder-gray-900 rounded-xl bg-white px-4 py-2 pl-8">
+            <div class="absolute top-0 flex items-center h-full ml-2">
+                <svg class="w-4 text-gray-700" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
+                </svg>
+            </div>
+        </div>
+    </div>
+    {{-- end filters --}}
+
+    <div class="ideas-container space-y-6 my-6">
+        <div class="idea-container hover:shadow-card transition duration-150 ease-in bg-white rounded-xl flex cursor-pointer">
+            <div class="border-r border-gray-100 px-5 py-8">
+                <div class="text-center">
+                    <div class="font-semibold text-2xl">12</div>
+                    <div class="text-gray-500">Votes</div>
+                </div>
+
+                <div class="mt-8">
+                    <button class="w-20 bg-gray-200 font-bold border border-gray-200 hover:border-gray-400 transition duration-150 ease-in text-xxs uppercase rounded-xl px-4 py-3">Vote</button>
+                </div>
+            </div>
+            <div class="flex px-2 py-6">
+                <a href="" class="flex-none">
+                    <img src="https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8YXZhdGFyfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60" alt="" class="w-14 h-14 rounded-xl">
+                </a>
+                <div class="mx-4">
+                    <h4 class="text-xl font-semibold">
+                        <a href="#" class="hover:underline">A random title can go here</a>
+                    </h4>
+                    <div class="text-gray-600 mt-3 line-clamp-3">
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Soluta ipsum nemo laudantium facilis minima magnam, explicabo eveniet sapiente. Debitis, natus totam reprehenderit cumque aliquam facilis! Excepturi iste rem perspiciatis! Doloribus expedita exercitationem dolorum beatae, dignissimos vitae consequuntur soluta necessitatibus quam a facilis sed asperiores amet. Sunt dolorem excepturi maxime at eius facere accusantium amet culpa quia! Eligendi voluptates voluptas, quibusdam laudantium aspernatur quos quis nemo? Recusandae eaque cupiditate perspiciatis tempora harum aspernatur minima, consequuntur laudantium reiciendis maiores nulla assumenda magnam voluptate. Vero qui hic mollitia quidem ullam quam minima, repellendus quasi nisi delectus voluptates laboriosam, dolore sapiente labore libero non!
+                    </div>
+
+                    <div class="flex items-center justify-between mt-6">
+                        <div class="flex items-center text-xs font-semibold space-x-2 text-gray-400">
+                            <div>10 hours ago</div>
+                            <div>&bull;</div>
+                            <div>Category 1</div>
+                            <div>&bull;</div>
+                            <div class="text-gray-900">3 Comments</div>
+                        </div>
+                        <div class="flex items-center space-x-2">
+                            <div class="bg-gray-200 text-xxs font-bold uppercase leading-none rounded-full text-center w-28 h-7 py-2 px-4">Open</div>
+                            <button class="relative bg-gray-100 hover:bg-gray-200 rounded-full h-7 transition duration-150 ease-in py-2 px-3">
+                                <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 12h.01M12 12h.01M19 12h.01M6 12a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0z" />
+                                </svg>
+                                <ul class="absolute w-44 text-left font-semibold bg-white shadow-dialog rounded-xl py-3 ml-8">
+                                    <li><a href="" class="hover:bg-gray-100 block transition duration-150 ease-in px-5 py-3">Mark as Spam</a></li>
+                                    <li><a href="" class="hover:bg-gray-100 block transition duration-150 ease-in px-5 py-3">Delete Post</a></li>
+                                </ul>
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</x-app-layout>
