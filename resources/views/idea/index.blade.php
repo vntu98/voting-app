e50e749498c31415d263d4db310d3de289c120eb
vntu98 <vntu98@gmail.com>
2021-10-03 15:14:58 +0700

    Idea Container Link

--- resources/views/idea/index.blade.php
+++ resources/views/idea/index.blade.php
@@ -27,7 +27,20 @@
     </div> <!-- end filters -->
     <div class="ideas-container space-y-6 my-8">
         @foreach ($ideas as $idea)
-            <div class="idea-container hover:shadow-card transition duration-150 ease-in bg-white rounded-xl flex cursor-pointer">
+            <div
+                x-data
+                @click="
+                    const clicked = $event.target
+                    const target = clicked.tagName.toLowerCase()
+
+                    const ignores = ['button', 'svg', 'path', 'a']
+                    
+                    if (!ignores.includes(target)) {
+                        $event.target.closest('.idea-container').querySelector('.idea-link').click()
+                    }
+                " 
+                class="idea-container hover:shadow-card transition duration-150 ease-in bg-white rounded-xl flex cursor-pointer"
+            >
                 <div class="hidden md:block border-r border-gray-100 px-5 py-8">
                     <div class="text-center">
                         <div class="font-semibold text-2xl">12</div>
@@ -46,7 +59,7 @@
                     </div>
                     <div class="w-full flex flex-col justify-between mx-2">
                         <h4 class="text-xl font-semibold mt-2 md:mt-0">
-                            <a href="{{ route('idea.show', $idea)  }}" class="hover:underline">{{ $idea->title }}</a>
+                            <a href="{{ route('idea.show', $idea)  }}" class="idea-link hover:underline">{{ $idea->title }}</a>
                         </h4>
                         <div class="text-gray-600 mt-3 line-clamp-3">
                             {{ $idea->description }}
