497f7343cedd85e958bb8fed2202555e6035b7c3
vntu98 <vntu98@gmail.com>
2021-10-03 11:41:19 +0700

    Dialog visibility with apline

--- tailwind.config.js
+++ tailwind.config.js
@@ -25,8 +25,11 @@ module.exports = {
                 'purple': '#8b60ed',
             },
             spacing: {
+                22: '5.5rem',
                 44: '11rem',
                 70: '17.5rem',
+                76: '19rem',
+                104: '26rem',
                 175: '43.75rem',
             },
             maxWidth: {
