82e4cdab3a2e3c11cc713c1f62546a0665a737d9
vntu98 <vntu98@gmail.com>
2021-10-06 22:04:53 +0700

    edit comments

--- app/Http/Livewire/IdeaComments.php
+++ app/Http/Livewire/IdeaComments.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Livewire;
 
+use App\Models\Comment;
 use App\Models\Idea;
 use Livewire\Component;
 use Livewire\WithPagination;
@@ -28,7 +29,11 @@ class IdeaComments extends Component
     public function render()
     {
         return view('livewire.idea-comments', [
-            'comments' => $this->idea->comments()->paginate()->withQueryString(),
+//            'comments' => $this->idea->comments()->paginate()->withQueryString(),
+            'comments' => Comment::with('user')
+                ->where('idea_id', $this->idea->id)
+                ->paginate()
+                ->withQueryString()
         ]);
     }
 }
--- app/Policies/CommentPolicy.php
+++ app/Policies/CommentPolicy.php
@@ -53,7 +53,7 @@ class CommentPolicy
      */
     public function update(User $user, Comment $comment)
     {
-        //
+        return (int) $comment->user_id === $user->id;
     }
 
     /**
