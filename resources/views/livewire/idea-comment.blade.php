22bf6b0f5c2388d8769513432ac5d9a2fa0915f0
vntu98 <vntu98@gmail.com>
2021-10-07 21:08:00 +0700

    set status comment ui

--- resources/views/livewire/idea-comment.blade.php
+++ resources/views/livewire/idea-comment.blade.php
@@ -1,28 +1,43 @@
-<div class="comment-container relative bg-white rounded-xl flex transition duration-500 ease-in mt-4">
+<div
+    class="@if ($comment->is_status_update) is-status-update {{ 'status-'.Str::kebab($comment->status->name)}}@endif comment-container relative bg-white rounded-xl flex transition duration-500 ease-in mt-4"
+>
     <div class="flex flex-col md:flex-row flex-1 px-4 py-6">
         <div class="flex-none">
             <a href="#">
                 <img src="{{ $comment->user->getAvatar() }}" alt="avatar" class="w-14 h-14 rounded-xl">
             </a>
+            @if ($comment->user->isAdmin())
+                <div class="md:text-center uppercase text-blue text-xxs font-bold mt-1">Admin</div>
+            @endif
         </div>
         <div class="w-full md:mx-4">
             <div class="text-gray-600">
                 @admin
-                    @if ($comment->spam_reports > 0)
-                        <div class="text-red mb-2">Spam Reports: {{ $comment->spam_reports }}</div>
-                    @endif
+                @if ($comment->spam_reports > 0)
+                    <div class="text-red mb-2">Spam Reports: {{ $comment->spam_reports }}</div>
+                @endif
                 @endadmin
-                {{ $comment->body }}
+
+                @if ($comment->is_status_update)
+                    <h4 class="text-xl font-semibold mb-3">
+                        Status Changed to "{{ $comment->status->name }}"
+                    </h4>
+                @endif
+
+                <div class="mt-4 md:mt-0">
+                    {{ $comment->body }}
+                </div>
             </div>
 
             <div class="flex items-center justify-between mt-6">
                 <div class="flex items-center text-xs text-gray-400 font-semibold space-x-2">
-                    <div class="font-bold text-gray-900">{{ $comment->user->name }}</div>
-                    @if ($comment->user_id === $ideaUserId)
-                        <div>&bull;</div>
-                        <div class="rounded-full border bg-gray-100 px-3 py-1">OP</div>
-                    @endif
+                    <div class="@if ($comment->is_status_update) text-blue @endif font-bold text-gray-900">{{ $comment->user->name }}</div>
                     <div>&bull;</div>
+                    {{-- @if ($comment->user->id === $comment->idea->user->id) --}}
+                    @if ($comment->user->id === $ideaUserId)
+                        <div class="rounded-full border bg-gray-100 px-3 py-1">OP</div>
+                        <div>&bull;</div>
+                    @endif
                     <div>{{ $comment->created_at->diffForHumans() }}</div>
                 </div>
                 @auth
@@ -49,29 +64,31 @@
                                         <a
                                             href="#"
                                             @click.prevent="
-                                                isOpen = false
-                                                Livewire.emit('setEditComment', {{ $comment->id }})
-                                            "
+                                        isOpen = false
+                                        Livewire.emit('setEditComment', {{ $comment->id }})
+                                    "
                                             class="hover:bg-gray-100 block transition duration-150 ease-in px-5 py-3"
                                         >
                                             Edit Comment
                                         </a>
                                     </li>
                                 @endcan
+
                                 @can('delete', $comment)
                                     <li>
                                         <a
                                             href="#"
                                             @click.prevent="
-                                            isOpen = false
-                                            Livewire.emit('setDeleteComment', {{ $comment->id }})
-                                                "
+                                        isOpen = false
+                                        Livewire.emit('setDeleteComment', {{ $comment->id }})
+                                    "
                                             class="hover:bg-gray-100 block transition duration-150 ease-in px-5 py-3"
                                         >
                                             Delete Comment
                                         </a>
                                     </li>
                                 @endcan
+
                                 <li>
                                     <a
                                         href="#"
@@ -84,21 +101,22 @@
                                         Mark as Spam
                                     </a>
                                 </li>
+
                                 @admin
-                                    @if ($comment->spam_reports > 0)
-                                        <li>
-                                            <a
-                                                href="#"
-                                                @click.prevent="
+                                @if ($comment->spam_reports > 0)
+                                    <li>
+                                        <a
+                                            href="#"
+                                            @click.prevent="
                                             isOpen = false
                                             Livewire.emit('setMarkAsNotSpamComment', {{ $comment->id }})
                                         "
-                                                class="hover:bg-gray-100 block transition duration-150 ease-in px-5 py-3"
-                                            >
-                                                Not Spam
-                                            </a>
-                                        </li>
-                                    @endif
+                                            class="hover:bg-gray-100 block transition duration-150 ease-in px-5 py-3"
+                                        >
+                                            Not Spam
+                                        </a>
+                                    </li>
+                                @endif
                                 @endadmin
                             </ul>
                         </div>
